7f64aba1c3c1f058759e71af3879618d486160ef
29yerin-tech <29yerin@gmail.com>
2026-05-20 23:15:22 +0900

    .

---
+++ WheeledLab/WheeledLab/source/wheeledlab_assets/wheeledlab_assets/__init__.py
@@ -0,0 +1,20 @@
+# 에셋(로봇/지형) 폴더 경로와 버전 정보를 한 곳에 모아둠.
+
+import os
+import toml
+
+# Conveniences to other module directories via relative paths
+WHEELEDLAB_ASSETS_EXT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+"""Path to the extension source directory."""
+
+WHEELEDLAB_ASSETS_DATA_DIR = os.path.join(WHEELEDLAB_ASSETS_EXT_DIR, "data")
+"""Path to the extension data directory."""
+
+WHEELEDLAB_ASSETS_METADATA = toml.load(os.path.join(WHEELEDLAB_ASSETS_EXT_DIR, "config", "extension.toml"))
+"""Extension metadata dictionary parsed from the extension.toml file."""
+
+# Configure the module-level variables
+__version__ = WHEELEDLAB_ASSETS_METADATA["package"]["version"]
+
+from .mushr import *
+from .f1tenth import *
---
+++ WheeledLab/WheeledLab/source/wheeledlab_tasks/wheeledlab_tasks/__init__.py
@@ -0,0 +1,66 @@
+import gymnasium as gym
+
+########################################
+############ DRIFT ENVS ################
+########################################
+# 환경 이름 사전 등록:
+# "Isaac-MushrDriftRL-v0" 같은 이름 붙여놓음.
+# 그래서 나중에 gym.make("그이름") 가능.
+
+from .drifting import MushrDriftRLEnvCfg, MushrDriftPlayEnvCfg
+from .visual import MushrVisualRLEnvCfg, MushrVisualPlayEnvCfg
+from .elevation import MushrElevationRLEnvCfg, MushrElevationPlayEnvCfg
+import wheeledlab_tasks.drifting.config.agents.mushr as mushr_drift_agents
+import wheeledlab_tasks.visual.config.agents.mushr as mushr_visual_agents
+import wheeledlab_tasks.elevation.config.agents.mushr as mushr_elevation_agents
+
+gym.register(
+    id="Isaac-MushrDriftRL-v0",
+    entry_point='isaaclab.envs:ManagerBasedRLEnv',
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point":MushrDriftRLEnvCfg,
+        "rsl_rl_cfg_entry_point": f"{mushr_drift_agents.__name__}.rsl_rl_ppo_cfg:MushrPPORunnerCfg",
+        "play_env_cfg_entry_point": MushrDriftPlayEnvCfg
+    }
+)
+
+
+gym.register(
+    id="Isaac-MushrVisualRL-v0",
+    entry_point='isaaclab.envs:ManagerBasedRLEnv',
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point":MushrVisualRLEnvCfg,
+        "rsl_rl_cfg_entry_point": f"{mushr_visual_agents.__name__}.rsl_rl_ppo_cfg:MushrPPORunnerCfg",
+        "play_env_cfg_entry_point": MushrVisualPlayEnvCfg
+    }
+)
+
+gym.register(
+    id="Isaac-MushrElevationRL-v0",
+    entry_point='isaaclab.envs:ManagerBasedRLEnv',
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": MushrElevationRLEnvCfg,
+        "rsl_rl_cfg_entry_point": f"{mushr_elevation_agents.__name__}.rsl_rl_ppo_cfg:MushrPPORunnerCfg",
+        "play_env_cfg_entry_point": MushrElevationPlayEnvCfg
+    }
+)
+
+#######################################
+############ F1TENTH ENVS #############
+#######################################
+
+from .drifting import F1TenthDriftRLEnvCfg
+import wheeledlab_tasks.drifting.config.agents.f1tenth as f1tenth_drift_agents
+
+gym.register(
+    id="Isaac-F1TenthDriftRL-v0",
+    entry_point='isaaclab.envs:ManagerBasedRLEnv',
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point":F1TenthDriftRLEnvCfg,
+        "rsl_rl_cfg_entry_point": f"{f1tenth_drift_agents.__name__}.rsl_rl_ppo_cfg:F1TenthPPORunnerCfg",
+    }
+)
