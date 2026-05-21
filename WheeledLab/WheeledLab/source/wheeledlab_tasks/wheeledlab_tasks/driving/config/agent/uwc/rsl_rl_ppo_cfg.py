06a0c0ed081b627fd27ed8f5e0c66eeb8af5d15e
autovehicle <jskim010903@inha.edu>
2026-05-21 09:01:06 +0900

    Fixed: rsl_rl_ppo_cfg.py hyperparameter 주석 완료 (추후 hyperparam 조정할때 필요)

--- WheeledLab/WheeledLab/source/wheeledlab_tasks/wheeledlab_tasks/driving/config/agent/uwc/rsl_rl_ppo_cfg.py
+++ WheeledLab/WheeledLab/source/wheeledlab_tasks/wheeledlab_tasks/driving/config/agent/uwc/rsl_rl_ppo_cfg.py
@@ -24,16 +24,16 @@ class UWCDrivePPORunnerCfg(RslRlOnPolicyRunnerCfg):
     )
 
     algorithm = RslRlPpoAlgorithmCfg(
-        value_loss_coef=1.0,                # Critic
-        use_clipped_value_loss=True,
-        clip_param=0.2,
-        entropy_coef=0.005,
-        num_learning_epochs=5,
-        num_mini_batches=4,
-        learning_rate=5.0e-4,
-        schedule="adaptive",
-        gamma=0.99,
-        lam=0.95,
-        desired_kl=0.01,
-        max_grad_norm=1.0,
+        value_loss_coef=1.0,                # Critic 실수에서 얼마나 penalty를 줄지 결정 (해당 값이 높을수록 critic교육에 더 집중)
+        use_clipped_value_loss=True,        # 한 번에 너무 크게 업데이트 되는 것을 방지
+        clip_param=0.2,                     # 한 번에 너무 크게 업데이트 되는 것을 방지 (clip_param이 0.2면, 행동 확률이 20% 이상 바뀌는 것을 방지)
+        entropy_coef=0.005,                 # 행동의 다양성 유지를 위해 entropy에 penalty 주는 parameter (값이 높을수록 행동의 다양성 유지에 더 집중)
+        num_learning_epochs=5,              # epoch: 128회의 경험을 몇 번 복습할건지 결정
+        num_mini_batches=4,                 # mini-batch: 128회의 경험을 몇 개의 작은 batch로 나눌건지 결정 (128회 경험을 4개로 나누면, 한 mini-batch는 32회 경험으로 구성)
+        learning_rate=5.0e-4,               # learning rate: 신경망이 얼마나 빠르게 업데이트 되는지 결정 (값이 너무 높으면 학습이 불안정해지고, 너무 낮으면 학습이 느려짐)
+        schedule="adaptive",                # learning rate를 adaptive하게 조절 (KL divergence가 desired_kl보다 크면 learning rate를 줄이고, 작으면 learning rate를 늘리는 방식)
+        gamma=0.99,                         # 미래 보상에 대한 할인율 (값이 0.99면, 1초 후의 보상보다 100초 후의 보상이 100배 더 작게 평가됨)
+        lam=0.95,                           # 이득 계산을 얼마나 더 멀리 볼지 결정 (값이 0.95면, 1초 후의 보상보다 20초 후의 보상이 100배 더 작게 평가됨)
+        desired_kl=0.01,                    # learning rate를 adaptive하게 조절할 때 사용하는 parameter (KL divergence가 desired_kl보다 크면 learning rate를 줄이고, 작으면 learning rate를 늘리는 방식)
+        max_grad_norm=1.0,                  # gradient clipping: 한 번에 너무 크게 업데이트 되는 것을 방지 (값이 1.0이면, gradient의 L2 norm이 1.0을 넘지 않도록 클리핑)
     )
